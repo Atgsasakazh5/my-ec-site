5661d72f5e1307539b730e567a26d1e68311538a
Kazuki Sasaki <sasakazh5@gmail.com>
2025-06-30 21:58:12 +0900

    最初のコミット

---
+++ src/main/java/com/github/Atgsasakazh5/my_ec_site/MyEcSiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.Atgsasakazh5.my_ec_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEcSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEcSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Atgsasakazh5/my_ec_site/MyEcSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Atgsasakazh5.my_ec_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEcSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
