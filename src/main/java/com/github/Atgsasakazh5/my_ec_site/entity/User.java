a38d8526f0d416bfde4865ab60d36fd6329fa150
Kazuki Sasaki <sasakazh5@gmail.com>
2025-07-02 23:45:02 +0900

    Userのentityを作成

---
+++ src/main/java/com/github/Atgsasakazh5/my_ec_site/entity/User.java
@@ -0,0 +1,50 @@
+package com.github.Atgsasakazh5.my_ec_site.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)  // JPA Auditingを有効化
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @ToString.Exclude   // ログにパスワードが出力されないようにする
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private boolean subscribingNewsletter;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
