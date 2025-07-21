97d4b59e89b2d9bfb36b5962d3d4fd945fd4ae8d
Kazuki Sasaki <sasakazh5@gmail.com>
2025-07-21 23:09:52 +0900

    JWT認証機能の追加: JwtProviderクラスを実装し、application.propertiesにJWT設定を追加

---
+++ src/main/java/com/github/Atgsasakazh5/my_ec_site/security/JwtProvider.java
@@ -0,0 +1,73 @@
+package com.github.Atgsasakazh5.my_ec_site.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Value("${app.JWT_SECRET}")
+    private String jwtSecret;
+
+    @Value("${app.JWT_EXPIRATION_MS}")
+    private int jwtExpirationMs;
+
+    // トークンを発行
+    public String generateToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationMs);
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key())
+                .compact();
+    }
+
+    // トークンからユーザー名を抽出するメソッド
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // トークンの有効性を検証するメソッド
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(key()).build().parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    private SecretKey key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+}
